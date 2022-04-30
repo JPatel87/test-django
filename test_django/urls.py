d9d3fc63f48954c4b8f7f5e1245d2b62a39a417f
JPatel87 <jaime_patel@hotmail.co.uk>
2022-04-30 16:05:13 +0000

    test code

--- test_django/urls.py
+++ test_django/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from home.views import get_home_page
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', get_home_page, name='hello')
 ]
